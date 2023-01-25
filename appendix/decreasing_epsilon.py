updated epsilon to correct formula

appendix/decreasing_epsilon.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import gamma
 
 plt.style.use('seaborn-whitegrid')
 plt.rcParams.update({
@@ -9,22 +10,33 @@ plt.rcParams.update({
 })
 
 
-def eps_m(n, m, p, dV=1, xi=1):
-    return (dV/xi)*(1 - (p/n)**(1/m))
+d = 2
+
+def eps_m(n, m, p, d=2, dV=1):
+    xi = np.pi**(d/2) / gamma((d/2) + 1)
+    return ((dV/xi)*(1 - (p/n)**(1/m)))**(1/d)
 
 
-mseq = np.arange(1, 200, 5)
-nseq = np.arange(1, 200, 5)**4
+mseq = np.arange(1, 30*d**2, d**2/4)
+nseq = np.arange(1, 30*d**2, d**2/4)**3
 
 p = 0.95
 
 fig, ax = plt.subplots(1, 4, figsize=(12, 3))
 for i, n in enumerate(nseq[:4]):
-    ax[i].plot(mseq, eps_m(n, mseq, 1-p), linewidth=3)
+    ax[i].plot(mseq, eps_m(int(n), mseq.round(0), 1-p, d), linewidth=3)
     ax[i].set_xlabel("$m$")
     ax[i].set_ylabel("$\\varepsilon_m$")
-    ax[i].set_title("$n_{\\varepsilon_m}" + f"={n}$")
-    ax[i].set_ylim(0, 1)
+    ax[i].set_title("$n_{\\varepsilon_m}" + f"={int(n)}$")
 fig.tight_layout()
 
+ax[0].text(
+    -0.6, 0.5, f"$d={d}$",
+    horizontalalignment='center',
+    verticalalignment='center',
+    fontsize = "large",
+    transform=ax[0].transAxes
+)
+
+plt.savefig(f"epsilon_d{d}.pdf", bbox_inches="tight")
 plt.show()

library/tksd_u_statistic.py
@@ -48,7 +48,7 @@ def tksd(X, Px, dlogp, bandwidth=None, theta_init=None, plot_g=False):
     K, K_y, K_xy, Kyt, Kyt_x, Kxtyt = kgof_make_Ks_all(X, Px, bandwidth)
     Kxtyt_inv = cholesky_inv(Kxtyt, 1e-3)
 
-    # for U-statistic, i\neq j, so make diagonal entries zero for samples n
+    # for U-statistic, i \neq j, so make diagonal entries zero for samples n
     np.fill_diagonal(K, 0)
     np.fill_diagonal(K_xy, 0)
     for l in range(d):
@@ -60,7 +60,6 @@ def tksd(X, Px, dlogp, bandwidth=None, theta_init=None, plot_g=False):
     # Initial conditions are default random
     if theta_init is None:
         theta_init = np.random.randn(d)
-    n_p = len(theta_init)
 
     # Objective as a function of theta only
     obj0 = lambda par: obj_vec(X, par, dlogp, K, K_y, K_xy0, Kyt, Kyt_x, Kxtyt_inv)
@@ -74,9 +73,7 @@ def tksd(X, Px, dlogp, bandwidth=None, theta_init=None, plot_g=False):
     if plot_g:
         from library.tksd_g import g_
         g = "must be d<=2 for plot_g=True"
-        if d == 1:
-            g = g_(X, Px, dlogp, theta, K, K_y, Kyt, Kyt_x, Kxtyt, Kxtyt_inv, bandwidth);
-        if d == 2:
+        if d <= 2:
             g = g_(X, Px, dlogp, theta, K, K_y, Kyt, Kyt_x, Kxtyt, Kxtyt_inv, bandwidth);
         return theta, g
     else: