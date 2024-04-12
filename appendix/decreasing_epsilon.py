df22a4952ee3313a666037df3a050efd5808461a
Danny James Williams <56155783+dannyjameswilliams@users.noreply.github.com>
2024-04-12 14:55:42 +0100

    updated epsilon_m to lower bound

--- appendix/decreasing_epsilon.py
+++ appendix/decreasing_epsilon.py
@@ -5,29 +5,30 @@ from scipy.special import gamma
 plt.style.use('seaborn-whitegrid')
 plt.rcParams.update({
     "text.usetex": True,
-    "font.family": "Helvetica",
+    "font.family": "serif",
     "font.size": 18
 })
 
 
 d = 2
 
-def eps_m(n, m, p, d=2, dV=1):
+def lower_bound(n, m, p, d=2, dV=1):
     xi = np.pi**(d/2) / gamma((d/2) + 1)
-    return ((dV/xi)*(1 - (p/n)**(1/m)))**(1/d)
-
+    return ((dV/xi)*(1 - (p)**(1/m)))**(1/d)
 
 mseq = np.arange(1, 30*d**2, d**2/4)
 nseq = np.arange(1, 30*d**2, d**2/4)**3
+LVseq = np.arange(1, 30*d**2, d**2/4)**2
 
 p = 0.95
 
 fig, ax = plt.subplots(1, 4, figsize=(12, 3))
-for i, n in enumerate(nseq[:4]):
-    ax[i].plot(mseq, eps_m(int(n), mseq.round(0), 1-p, d), linewidth=3)
+for i, LV in enumerate(LVseq[:4]):
+    ax[i].plot(mseq, lower_bound(1, mseq.round(0), 1-p, d, dV=LV), linewidth=3, color="blue", label="Lower bound")
     ax[i].set_xlabel("$m$")
-    ax[i].set_ylabel("$\\varepsilon_m$")
-    ax[i].set_title("$n_{\\varepsilon_m}" + f"={int(n)}$")
+    ax[i].set_title("$\mathrm{L}(V)" + f"={int(LV)}$")
+
+ax[0].set_ylabel("Lower bound on $\\varepsilon_m$")
 fig.tight_layout()
 
 ax[0].text(
